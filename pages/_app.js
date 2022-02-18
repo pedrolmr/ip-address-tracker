0125c96a7fa665424d71348d76a5b95c75d720f2
pedrolmr <pedroluismontesinosrivera@gmail.com>
2022-02-18 11:23:24 -0500

    push

--- pages/_app.js
+++ pages/_app.js
@@ -15,17 +15,17 @@ function MyApp({ Component, pageProps }) {
           rel="stylesheet"
           href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-          crossorigin=""
+          crossOrigin=""
         />
         <script
           src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
           integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-          crossorigin=""
+          crossOrigin=""
         ></script>
       </Head>
       <Component {...pageProps} />
     </>
   );
 }
-
+//kflkfl
 export default MyApp;
